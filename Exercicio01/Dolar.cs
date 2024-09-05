c899ab779e4c5ad55de3b4441838db5492bab2c9
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2024-09-05 19:26:10 -0300

    Programa para converter dolar em reais

---
+++ Exercicio01/Dolar.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Globalization; 
+
+namespace Exercicio01 {
+    internal class Dolar {
+
+        public static double Reais;
+        public static double ValorComprado;
+       
+       public static double ValoraserPago() {
+            return Reais * ValorComprado;
+        }
+        
+        public static double IOF() {
+            return ValoraserPago() * 6.0/100.0;
+        }
+
+        public static double ValorFinal() {
+            return ValoraserPago() + IOF();
+        }
+    }
+}
---
+++ Exercicio01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio01 {
+    class Program {
+        static void Main(string[] args) {
+
+            
+
+            Console.WriteLine("Qual é a cotação do dólar?: ");
+            Dolar.Reais = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Quantos dólares você vai comprar?: ");
+            Dolar.ValorComprado = double.Parse (Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = " +Dolar.ValorFinal(),CultureInfo.InvariantCulture);
+
+
+
+
+
+
+        }
+    }
+}
